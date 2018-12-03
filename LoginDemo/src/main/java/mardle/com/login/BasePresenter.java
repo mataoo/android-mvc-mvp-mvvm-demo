97332507ad2b7280c2fd24d111737c3704a20e09
matao <muxter.matao@gmail.com>
2018-12-03 23:54:11 +0800

    build: upgrade build tool

--- LoginDemo/src/main/java/mardle/com/login/BasePresenter.java
+++
@@ -1,8 +0,0 @@
-package mardle.com.login;
-
-/**
- * Created by matao on 2016-05-09 11:50
- */
-public interface BasePresenter{
-    void start();
-}
--- LoginDemo/src/main/java/mardle/com/login/login/model/IUserBiz.java
+++
@@ -1,8 +0,0 @@
-package mardle.com.login.login.model;
-
-/**
- * Created by matao on 2016-05-09 14:24
- */
-public interface IUserBiz {
-    void login(String username, String password, OnLoginListener onLoginListener);
-}
